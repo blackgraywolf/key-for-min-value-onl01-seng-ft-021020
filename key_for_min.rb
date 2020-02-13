34d29b8bb02f0192e015f3b9322be0cb6206ba9e
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-02-13 20:47:05 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,10 +4,10 @@
 def key_for_min_value(name_hash)
   lowest_key = nil 
   lowest_value = nil 
-  name_hash.each do |k, v|
-    if lowest_value == nil || v < lowest_value
-      lowest_value = v 
-      lowest_key = k 
+  name_hash.each do |low, high|
+    if lowest_value == nil || high < lowest_value
+      lowest_value = high 
+      lowest_key = low 
     end 
   end 
   lowest_key
